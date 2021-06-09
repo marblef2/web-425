b2af93988b1c34da461df64088e0321db432e2c3
Fred Marble <fmarble@my365.bellevue.edu>
2021-06-09 16:15:28 -0500

    Adding in Assignment 3.4

---
+++ week-3/enhanced-secure-profile-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Enhanced Profile App';
+  assignment: String = 'Marble Exercise 3.3 - Passing Data to Routes, Part 2';
+}
---
+++ week-3/enhanced-secure-profile-app/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import {Routes} from '@angular/router';
+import {SignInComponent} from './sign-in/sign-in.component';
+import {HomeComponent} from './home/home.component';
+import {SignInGuard} from './sign-in.guard'
+
+export const AppRoutes: Routes = [
+  {
+    path: '',
+    component: SignInComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate:[SignInGuard]
+  }
+]
