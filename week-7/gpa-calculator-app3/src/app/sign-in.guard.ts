e8212a02d409ce358003aa7a41092568b8d0ca9b
Fred Marble <fmarble@my365.bellevue.edu>
2021-07-05 16:44:13 -0500

    Adding in Week 7, and Exercise 7.2

---
+++ week-7/gpa-calculator-app3/src/app/sign-in.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignInGuard implements CanActivate {
+
+  constructor(private router: Router, private cookieService: CookieService){}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+
+    const sessionUser = this.cookieService.get('session_user');
+
+    if(sessionUser){
+      return true;
+    } else{
+      this.router.navigate(['/session/sign-in']);
+      return false
+    }
+  }
+
+}
